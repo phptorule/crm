f809c9beb5193270abce03fa44dc93d03cc25cf8
Denys Domanskyi <manager@div-art.com>
2017-10-17 16:33:59 +0300

    Change colors for temaplate, edit some text to polish

--- resources/views/customers/list.blade.php
+++ resources/views/customers/list.blade.php
@@ -3,14 +3,14 @@
       <div class="panel panel-bd">
          <div class="panel-heading">
             <div class="btn-group" id="buttonexport">
-                  <h4>List of customers</h4>
+                  <h4>Lista kontrahentów</h4>
             </div>
          </div>
          <div class="panel-body">
          <!-- Plugin content:powerpoint,txt,pdf,png,word,xl -->
             <div class="btn-group">
                <div class="buttonexport" id="buttonlist">
-                  <a class="btn btn-add" href="/customers/add"> <i class="fa fa-plus"></i> Add Customer
+                  <a class="btn btn-add" href="/customers/add"> <i class="fa fa-plus"></i> Utwórz Kontrahenta
                   </a>
                </div>
                <button class="btn btn-exp btn-sm dropdown-toggle" data-toggle="dropdown"><i class="fa fa-bars"></i> Export Table Data</button>
@@ -134,62 +134,6 @@
       </div>
    </div>
 </div>
-<!-- customer Modal1 -->
-<div class="modal fade" id="customer1" tabindex="-1" role="dialog" aria-hidden="true">
-   <div class="modal-dialog">
-      <div class="modal-content">
-         <div class="modal-header modal-header-primary">
-            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-            <h3><i class="fa fa-user m-r-5"></i> Update Customer</h3>
-         </div>
-         <div class="modal-body">
-            <div class="row">
-               <div class="col-md-12">
-                  <form class="form-horizontal">
-                     <fieldset>
-                        <!-- Text input-->
-                        <div class="col-md-4 form-group">
-                           <label class="control-label">Customer Name:</label>
-                           <input type="text" placeholder="Customer Name" class="form-control">
-                        </div>
-                        <!-- Text input-->
-                        <div class="col-md-4 form-group">
-                           <label class="control-label">Email:</label>
-                           <input type="email" placeholder="Email" class="form-control">
-                        </div>
-                        <!-- Text input-->
-                        <div class="col-md-4 form-group">
-                           <label class="control-label">Mobile</label>
-                           <input type="number" placeholder="Mobile" class="form-control">
-                        </div>
-                        <div class="col-md-6 form-group">
-                           <label class="control-label">Address</label><br>
-                           <textarea name="address" rows="3"></textarea>
-                        </div>
-                        <div class="col-md-6 form-group">
-                           <label class="control-label">type</label>
-                           <input type="text" placeholder="type" class="form-control">
-                        </div>
-                        <div class="col-md-12 form-group user-form-group">
-                           <div class="pull-right">
-                              <button type="button" class="btn btn-danger btn-sm">Cancel</button>
-                              <button type="submit" class="btn btn-add btn-sm">Save</button>
-                           </div>
-                        </div>
-                     </fieldset>
-                  </form>
-               </div>
-            </div>
-         </div>
-         <div class="modal-footer">
-            <button type="button" class="btn btn-danger pull-left" ng-click="cancel()">Close</button>
-         </div>
-      </div>
-      <!-- /.modal-content -->
-   </div>
-   <!-- /.modal-dialog -->
-</div>
-<!-- /.modal -->
 <!-- Modal -->
 <!-- Customer Modal2 -->
 <script type="text/ng-template" id="CustomersDelete.html">
