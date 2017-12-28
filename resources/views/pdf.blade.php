09cbe4626faaf0ae4aec1482150fd3b6675b47c9
Oleksandr Fedorchuk <of@div-art.com>
2017-12-28 13:21:40 +0200

    generate pdf final

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -1,13 +1,21 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="utf-8">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
         <link href="/css/app.css" rel="stylesheet" type="text/css"/>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.5/jspdf.min.js"></script>
         <script src="https://code.jquery.com/jquery-1.12.3.min.js"></script>
-        
+        <style>
+            #container{ font-family: DejaVu Sans, sans-serif; font-size: 12px;}
+            .customers_table td {
+                padding: 5px;
+                vertical-align: top;
+                text-transform: uppercase;
+                font-weight: 400;
+            }
+        </style>
     </head>
 
     <body>
@@ -23,7 +31,7 @@
                         </tr>
 
                         <tr>
-                            <td class="text-right">Adres:</td>
+                            <td class="text-right pdf_adress">Adres:</td>
                             <td>{{$team['teams_invoice_street']}}, {{$team['teams_invoice_postcode']}}, <br /> {{$team['teams_invoice_town']}}</td>
                         </tr>
 
